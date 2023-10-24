fc2811e6a76803492d3078bbbfbdbd5106b9f7b2
Victor Salazar <35509355+VictorSalazar777@users.noreply.github.com>
2023-10-24 12:02:34 -0500

    Add async

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../services/products.service';
+import { PeriodicElement } from '../services/periodicelement';
 import {MatTableModule} from '@angular/material/table';
 
 
@@ -12,7 +13,15 @@ import {MatTableModule} from '@angular/material/table';
   imports: [MatTableModule, CommonModule],
 })
 export class ProductsComponent {
+
   productsService: ProductsService = inject(ProductsService)
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = this.productsService.ELEMENT_DATA;
+  dataSource: PeriodicElement[] = []
+
+  constructor() {
+    this.productsService.getProducts().then((products: PeriodicElement[])=>
+      this.dataSource = products
+    )
+  }
+
 }
